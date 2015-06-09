b4ee14172f904d551656fac7e4928a3321ce115a
Daniel Crisp <daniel@danielcrisp.com>
2015-06-09 17:14:15 +0100

    Enabled livereload

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,10 +2,13 @@
 
     homeController.init = function (app) {
         app.get("/", function (req, res) {
+
             res.render("home/index", {
                 applicationName: "Web European Conference",
                 title: "Web European Conference",
-                csrfToken: req.csrfToken()
+                csrfToken: req.csrfToken(),
+                // embed the livereload script
+                livereload: GLOBAL.env === 'dev'
             });
         });
     };
